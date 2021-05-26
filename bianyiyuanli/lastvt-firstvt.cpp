32a20cb794d8bc686016233b23ac63a8958d8d31
谢怡星 <2628260069@qq.com>
2021-05-26 10:55:33 +0800

    用栈实现firstvt

--- bianyiyuanli/lastvt-firstvt.cpp
+++ bianyiyuanli/lastvt-firstvt.cpp
@@ -1,34 +1,37 @@
 #include <stdio.h>
-#include <math.h> 
+// #include <math.h> 
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
-#include <algorithm>
-#include <set>
-#include <queue>
 #include <stack>
-#include <map>
-#include <bitset>
-// typedef long long LL;
-// const int inf=0x3f3f3f3f;
-// const double pi= acos(-1.0);
-// const double esp=1e-6;
 using namespace std;
  
 const int Maxn=110;
 const int maxn=20;
 char str[maxn][Maxn];//输入文法
-// char st[maxn];//输入串
-// char stac[maxn];//模拟栈的数组
-// char nstr[maxn][maxn];//储存转化文法
-// char mstr[maxn][maxn];
 char fin[maxn];//存储终结符
 char firstvt[maxn][maxn],lastvt[maxn][maxn];
-// char cmp[maxn][maxn];//存储表中的比较符
 int firstflag[maxn],lastflag[maxn];//非终结符的firstvt,lastvt是否求出
 int fcnt[maxn],lcnt[maxn];//
+/*xyx*/
+bool F[maxn][maxn];//table
+int NTcount=0;
+int Tcount=0;
+char NT[maxn];
+char T[maxn];
+void out_firstvt();
+// stack<char,char> STACK;
+typedef struct pair_{
+    char NT;
+    char T;
+}pair_;
 
+stack<pair_*> STACK;
+
+// void insert(char NT, char T){
+//     // if(F[])
+// }
 int is_fin(char c) { //判断终结符
     for(int i=0; fin[i]!='\0'; i++) {
         if(fin[i]==c)
@@ -36,12 +39,81 @@ int is_fin(char c) { //判断终结符
     }
     return 0;
 }
-// int site(char c) { //求在表中的下标
-//     for(int i=0; fin[i]!='\0'; i++) {
-//         if(fin[i]==c)
-//             return i;
-//     }
-// }
+
+bool insert(int NTsite, int Tsite){
+    if(!F[NTsite][Tsite]){
+        F[NTsite][Tsite]=1;
+        pair_ *node = (pair_*)malloc(sizeof(pair_)) ;
+        node->NT = NT[NTsite];
+        node->T = T[Tsite];
+        STACK.push(node);
+        return 1;
+    }
+    return 0;
+}
+
+int getsite(char ch){
+    for(int i=0;i<Tcount;i++){
+        if(T[i]==ch){
+            return i;
+        }
+    }
+    cout<<"找不到该终结符"<<ch<<"的位置。";
+    return -1;
+}
+
+void do_firstvt(){ //char s,int t
+    int i,j,ii,jj,tt;
+    // for(i=0; i<t; i++) {//找到该非终结符对应的产生式
+    //     if(str[i][0]==s)
+    //         break;
+    // }
+    for(int i=0; i<NTcount;i++){ //对于每条产生式
+        for(j=0; str[i][j]!='\0'; j++) { //遍历这个产生式
+            if(j==2||str[i][j]=='|') { //跳过‘s->’的第一个候选式，下一个候选式
+                if(is_fin(str[i][j+1])) { //形如s->a，以终结符开头
+                    insert(i,getsite(str[i][j+1]));
+                } else { //以非终结符开头
+                    if(is_fin(str[i][j+2])) { //形如s->Ba,以非终结符开头，且紧跟着终结符
+                        insert(i,getsite(str[i][j+2]));
+                    }
+                }
+            }
+        }
+    }
+    out_firstvt();
+    char Q,a;
+    while(!STACK.empty()){
+        pair_ *node = STACK.top() ;
+        Q = node->NT;
+        a = node->T;
+        STACK.pop();
+        for(int i=0; i<NTcount;i++){ //对于每条产生式
+            for(j=0; str[i][j]!='\0'; j++) { //遍历这个产生式
+                if(j==2||str[i][j]=='|') { //跳过‘s->’的第一个候选式，下一个候选式
+                    if(Q == str[i][j+1]) { //形如s->a，以终结符开头
+                        insert(i,getsite(a));
+                }
+            }
+        }
+    }
+}}
+ //输出firstvt集
+
+void out_firstvt(){
+    for(int i=0;i<Tcount;i++){
+        cout<<"   "<<T[i];
+    }
+    cout<<endl;
+    for(int i=0;i<NTcount;i++){
+        cout<<NT[i]<<"  ";
+        for(int j=0;j<Tcount;j++){
+            cout<<F[i][j]<<"   ";
+        }
+        cout<<endl;
+    }
+}
+
 void get_firstvt(char s,int t) { //求s非终结符的firstvt值
     int i,j,ii,jj,tt;
     for(i=0; i<t; i++) {//找到该非终结符对应的产生式
@@ -82,7 +154,7 @@ void get_firstvt(char s,int t) { //求s非终结符的firstvt值
         firstflag[i]=1;
     }
 }
- 
+
 void output_firstvt(int T) { //输出firstvt集
     for(int i=0; i<T; i++) {
         get_firstvt(str[i][0],T); //输入每个产生式的第一个字符，也就是非终结符；以及产生式的个数
@@ -154,29 +226,46 @@ void output(int i,int j,char *str) {
     for(int ii=i; ii<=j; ii++)
         printf("%c",str[ii]);
 }
+
 int main() {
-    int T;
-    // while(scanf("%d",&T)!=EOF) //可以测试输入不同的文法规则集
-    // {
+    int Tnumber;
+    while(scanf("%d",&Tnumber)!=EOF) //可以测试输入不同的文法规则集
+    {
+        NTcount=Tnumber;//xyx 非终结符的数量
         int cnt=0;//终结符的个数
         memset(firstflag,0,sizeof(firstflag));
         memset(lastflag,0,sizeof(lastflag));
         // memset(cmp,0,sizeof(cmp));
+        memset(F,0,sizeof(F));
     
-        for(int i=0; i<T; i++) {
+        for(int i=0; i<Tnumber; i++) {
             scanf("%s",str[i]);
             fcnt[i]=lcnt[i]=0; //记录每个非终结符的firstvt/lastvt的个数
+            NT[i]=str[i][0]; //xyx 记录非终结符数组
         }
-        for(int i=0; i<T; i++) { //记录终结符数组
+        for(int i=0; i<Tnumber; i++) { //记录终结符数组
             for(int j=0; str[i][j]!='\0'; j++) {
-                if((str[i][j]<'A'||str[i][j]>'Z')&&(str[i][j]!='-'&&str[i][j]!='>')&&str[i][j]!='|')
+                if((str[i][j]<'A'||str[i][j]>'Z')&&(str[i][j]!='-'&&str[i][j]!='>')&&str[i][j]!='|'){
+                    T[cnt]=str[i][j];//xyx 记录终结符数组
                     fin[cnt++]=str[i][j];
+                }
             }
         }
+        T[cnt]='#';
         fin[cnt++]='#';
+        Tcount=cnt;//xyx 终结符数量
         fin[cnt]='\0';
-        output_firstvt(T);
-        output_lastvt(T);
-    // }
+        // output_firstvt(T);
+        // output_lastvt(T);
+        cout<<T<<endl;
+        cout<<NT<<endl;
+        do_firstvt();
+        out_firstvt();
+    }
     return 0;
 }
+/*
+3
+E->E+T|T
+T->T*F|F
+F->(E)|a*/
